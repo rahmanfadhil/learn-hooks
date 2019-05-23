842f4d9ff7276077bfb107513d7cc4b8499c6265
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-05-24 05:49:01 +0700

    Remove propsAreEqual function in Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,25 @@
 import React, { useState, useRef, memo } from "react";
 
-const Counter = memo(
-  ({ onOdd }) => {
-    const [counter, setCounter] = useState(0);
-    const renders = useRef(0);
+const Counter = memo(({ onOdd }) => {
+  const [counter, setCounter] = useState(0);
+  const renders = useRef(0);
 
-    return (
-      <div>
-        <div>Counter: {counter}</div>
-        <div>Renders: {renders.current++}</div>
-        <button
-          onClick={() => {
-            if (counter % 2 !== 0) {
-              onOdd();
-            }
-            setCounter(counter + 1);
-          }}
-        >
-          Increase Counter
-        </button>
-      </div>
-    );
-  },
-  (prev, next) => {
-    if (prev.data.hello !== next.data.hello) {
-      return false;
-    }
-
-    return true;
-  }
-);
+  return (
+    <div>
+      <div>Counter: {counter}</div>
+      <div>Renders: {renders.current++}</div>
+      <button
+        onClick={() => {
+          if (counter % 2 !== 0) {
+            onOdd();
+          }
+          setCounter(counter + 1);
+        }}
+      >
+        Increase Counter
+      </button>
+    </div>
+  );
+});
 
 export default Counter;
