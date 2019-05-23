4904306dd4e0f2577fd6472d28a00feccab651a0
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-05-24 05:59:32 +0700

    Use text2 as dependency in useMemo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Counter from "./Counter";
 
 function App() {
   const [text, setText] = useState();
+  const [text2, setText2] = useState();
 
   const onOdd = useCallback(() => setText(""), []);
-  const data = useMemo(() => ({ hello: "world" }), []);
+  const data = useMemo(() => ({ text2 }), [text2]);
 
   return (
     <div>
       <input type="text" onChange={e => setText(e.target.value)} value={text} />
+      <input
+        type="text"
+        onChange={e => setText2(e.target.value)}
+        value={text2}
+      />
       <Counter onOdd={onOdd} data={data} />
     </div>
   );
