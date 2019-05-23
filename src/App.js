d9f2442b163bb4f16fd8f6f5555d18fb562c8d43
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-05-24 05:48:33 +0700

    Use useMemo to memoize object

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import Counter from "./Counter";
 
 function App() {
   const [text, setText] = useState();
 
   const onOdd = useCallback(() => setText(""), []);
+  const data = useMemo(() => ({ hello: "world" }), []);
 
   return (
     <div>
       <input type="text" onChange={e => setText(e.target.value)} value={text} />
-      <Counter onOdd={onOdd} data={{ hello: "world" }} />
+      <Counter onOdd={onOdd} data={data} />
     </div>
   );
 }
